71a0bf413d301af55c67aa37d8439c1e63169919
Shrey2305 <shreysavani2305@gmail.com>
2022-09-19 15:59:29 +0530

    Updates

---
+++ precache-manifest.f1b248f5bc933f549a5aaeb53ea97add.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b121fe0d6e1f1de4a1f583c0a30cb7b7",
+    "url": "/20DCS105_Shrey/index.html"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/20DCS105_Shrey/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "5d6c330c3162f6696d72",
+    "url": "/20DCS105_Shrey/static/css/main.48551862.chunk.css"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/20DCS105_Shrey/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/20DCS105_Shrey/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6c330c3162f6696d72",
+    "url": "/20DCS105_Shrey/static/js/main.b51eb1d6.chunk.js"
+  },
+  {
+    "revision": "be2dd49f7226259a04af",
+    "url": "/20DCS105_Shrey/static/js/runtime-main.a0e436b5.js"
+  }
+]);
